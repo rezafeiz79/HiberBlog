5d762d71982e87a86cf40641153767884f30c268
rezafeiz79 <rezafeiz79@gmail.com>
2020-09-28 15:03:20 +0330

    Edited Category Class

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,3 +1,41 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
-    
+    @Id @GeneratedValue
+    private int id;
+    private String title;
+    private String description;
+
+    public Category(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
