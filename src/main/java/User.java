6f200bbfd3c1c8bd856182d4970008b906b93a8a
rezafeiz79 <rezafeiz79@gmail.com>
2020-09-28 15:00:11 +0330

    Edited User Class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,61 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id @GeneratedValue
+    private String id;
+    private String userName;
+    private String nationalCode;
+    private String birthDay;
+    private String password;
+
+    public User(String id, String userName, String nationalCode, String birthDay, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.nationalCode = nationalCode;
+        this.birthDay = birthDay;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
