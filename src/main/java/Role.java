6c841ab25c9c6b6ae1624441ecfe72e66729dbc9
rezafeiz79 <rezafeiz79@gmail.com>
2020-09-28 15:08:45 +0330

    Edited Role Class

--- src/main/java/Role.java
+++ src/main/java/Role.java
@@ -1,2 +1,33 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Role {
+    @Id @GeneratedValue
+    private Integer id;
+    private String title;
+
+    public Role(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Role() { }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
