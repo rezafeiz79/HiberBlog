bcdd7e9cdc05780a08d4cd78ed1109fc0811f8ab
rezafeiz79 <rezafeiz79@gmail.com>
2020-09-29 14:00:10 +0330

    Edited Main Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import org.hibernate.Session;
+
 public class Main {
     public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
 
+        session.save();
+
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
     }
 }
